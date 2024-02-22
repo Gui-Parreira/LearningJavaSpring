2e318718ea044b12601a6988e5dac6b62cea0f0d
Gui-Parreira <gui.parreira98@gmail.com>
2024-02-22 20:42:23 -0300

    Separando responsabilidades para busca

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,4 +1,24 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Principal {
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=ccf04810";
+    private  ConsumoApi consumo = new ConsumoApi();
+
+    public void exibeMenu() {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o nome da Série para busca: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO +
+                                        nomeSerie.replace(" ", "+") +
+                                        API_KEY);
+        //"https://www.omdbapi.com/?t=gilmore+girls&season=1&apikey=ccf04810"
+    }
+
 }
